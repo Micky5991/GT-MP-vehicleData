03dfbbd5b68158e5347005dda0bbf8d2409ec2cf
Francesco Paolocci (Micky5991) <micky5991@gmail.com>
2017-06-30 19:49:08 +0200

    vehicleData.json-Generator completed

---
+++ src/VDataGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Server.Managers;
+using GrandTheftMultiplayer.Shared;
+
+namespace vd_generator.server
+{
+    public class VDataGenerator : Script
+    {
+
+        public VDataGenerator()
+        {
+            API.consoleOutput("Starting vehicleData.json-generator...");
+
+            API.onClientEventTrigger += this.OnClientEvent;
+            API.onPlayerFinishedDownload += OnPlayerFinishedDownload;
+
+        }
+
+        private void OnPlayerFinishedDownload(Client player)
+        {
+            player.sendChatMessage("~b~To generate a new vehicleData.json, enter: /generatevd");
+        }
+        
+        public void OnClientEvent(Client sender, string eventname, object[] args)
+        {
+            if (eventname != "vd_receiveresult") return;
+            string result = Convert.ToString(args[0]);
+
+            try
+            {
+                
+                const string path = @"vd-generator/vehicleData.json";
+                Directory.CreateDirectory(Path.GetDirectoryName(path));
+                File.WriteAllText(path, result);
+                sender.sendChatMessage("vehicleData.json created successfully");
+            }
+            catch (IOException e)
+            {
+                sender.sendChatMessage("An error occured: " + e.Message);
+                API.consoleOutput(e.Message);
+                API.consoleOutput(e.StackTrace);
+            }
+            
+        }
+
+        [Command]
+        public void generatevd(Client sender)
+        {
+            sender.sendChatMessage("vehicleData.json-generation started...");
+
+            uint[] values = (uint[]) Enum.GetValues(typeof(VehicleHash));
+            sender.triggerEvent("vd_generate", API.toJson(values));   
+        }
+        
+    
+    }
+}
---
+++ src/generator.js
@@ -0,0 +1,27 @@
+API.onServerEventTrigger.connect(function(eventName, args) {
+    if(eventName !== "vd_generate") return;
+
+    var values = JSON.parse(args[0]);
+
+    var result = {};
+    for(var id of values) {
+        var row = {
+            DisplayName:            API.getVehicleDisplayName(id) , // API.returnNative("GET_DISPLAY_NAME_FROM_VEHICLE_MODEL", 4, id)
+            MaxSpeed:               API.returnNative("_GET_VEHICLE_MODEL_MAX_SPEED", 7, id),
+            MaxBraking:             API.returnNative("GET_VEHICLE_MODEL_MAX_BRAKING", 7, id),
+            MaxTraction:            API.returnNative("GET_VEHICLE_MODEL_MAX_TRACTION", 7, id),
+            MaxAcceleration:        API.returnNative("GET_VEHICLE_MODEL_ACCELERATION", 7, id),
+            _0xBFBA3BA79CFF7EBF:    API.returnNative("0xBFBA3BA79CFF7EBF", 7, id),
+            _0x53409B5163D5B846:    API.returnNative("0x53409B5163D5B846", 7, id),
+            _0xC6AD107DDC9054CC:    API.returnNative("0xC6AD107DDC9054CC", 7, id),
+            _0x5AA3F878A178C4FC:    API.returnNative("0x5AA3F878A178C4FC", 7, id),
+            MaxNumberOfPassengers:  (API.returnNative("0x2AD93716F184EDA4", 0, id) - 1),
+            MaxOccupants:           API.returnNative("0x2AD93716F184EDA4", 0, id),
+            VehicleClass:           API.returnNative("GET_VEHICLE_CLASS_FROM_NAME", 0, id)
+        };
+        result[id] = row;
+    }
+
+    API.triggerServerEvent("vd_receiveresult", API.toJson(result));
+
+});
